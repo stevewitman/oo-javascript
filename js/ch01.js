608c790f3be9fd87bcdf232edb157ff00c47929c
Steve Witman <stevewitman@gmail.com>
2015-11-13 21:59:32 -0700

    conver clocks to basic objects

--- js/ch01.js
+++ js/ch01.js
@@ -1,5 +1,26 @@
 function onReady() {
-	console.log('Hello');
+	var clock = createClock('clock');
+	var clock = createClock('clock2');
 }
 
+function createClock(id) {
+	var c = new Object();
+	c.updateClock = function() {
+		var date = new Date();
+		var clock = document.getElementById(id)
+		clock.innerHTML = formatDigits(date.getHours()) + ':' + formatDigits(date.getMinutes()) + ':' + formatDigits(date.getSeconds());
+	};
+	setInterval(c.updateClock, 1000);
+	c.updateClock();
+	return c;
+}
+
+
+
+function formatDigits(val) {
+	if (val < 10) {
+		val =  '0' + val;
+	}
+	return val;
+}
 window.onload = onReady;
