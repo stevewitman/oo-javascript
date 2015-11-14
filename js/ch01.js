5cd9367ded8281841423fda45e7df6bf7831208f
Steve Witman <stevewitman@gmail.com>
2015-11-14 14:10:47 -0700

    use constructor, use that to access this

--- js/ch01.js
+++ js/ch01.js
@@ -1,24 +1,24 @@
 function onReady() {
-	var clock = createClock('clock');
-	var clock = createClock('clock2');
+	var clock = new Clock('clock');
+	var clock = new Clock('clock2');
 }
 
-function createClock(id) {
-	var c = new Object();
-	c.updateClock = function() {
+function Clock(id) {
+	this.updateClock = function() {
 		var date = new Date();
 		var clock = document.getElementById(id)
 		clock.innerHTML = this.formatDigits(date.getHours()) + ':' + this.formatDigits(date.getMinutes()) + ':' + this.formatDigits(date.getSeconds());
 	};
-	c.formatDigits = function(val) {
+	this.formatDigits = function(val) {
 		if (val < 10) {
 			val =  '0' + val;
 		}
 		return val;
 	}
-	setInterval(function() {c.updateClock();}, 1000);
-	c.updateClock();
-	return c;
+	var that = this;
+	setInterval(function() {
+		that.updateClock();}, 1000);
+		this.updateClock();
 }
 
 
