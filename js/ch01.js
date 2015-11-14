56b95502a27d547d78f5ecaad216203e9aa25e03
Steve Witman <stevewitman@gmail.com>
2015-11-14 14:00:20 -0700

    move methods into object

--- js/ch01.js
+++ js/ch01.js
@@ -8,19 +8,20 @@ function createClock(id) {
 	c.updateClock = function() {
 		var date = new Date();
 		var clock = document.getElementById(id)
-		clock.innerHTML = formatDigits(date.getHours()) + ':' + formatDigits(date.getMinutes()) + ':' + formatDigits(date.getSeconds());
+		clock.innerHTML = this.formatDigits(date.getHours()) + ':' + this.formatDigits(date.getMinutes()) + ':' + this.formatDigits(date.getSeconds());
 	};
-	setInterval(c.updateClock, 1000);
+	c.formatDigits = function(val) {
+		if (val < 10) {
+			val =  '0' + val;
+		}
+		return val;
+	}
+	setInterval(function() {c.updateClock();}, 1000);
 	c.updateClock();
 	return c;
 }
 
 
 
-function formatDigits(val) {
-	if (val < 10) {
-		val =  '0' + val;
-	}
-	return val;
-}
+
 window.onload = onReady;
