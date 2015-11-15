6484e3a0d53193f66b41970004b1934674d5d034
Steve Witman <stevewitman@gmail.com>
2015-11-14 23:07:19 -0700

    add offset and label for more reusable object

--- js/ch01.js
+++ js/ch01.js
@@ -1,20 +1,30 @@
 function onReady() {
 	var clock = new Clock('clock');
-	var clock = new Clock('clock2');
+	var clock = new Clock('clock2', -300, 'ETC');
+	var clock = new Clock('clock3', 300, 'X');
 }
 
-function Clock(id) {
+
+function Clock(id, offset, label) {
+	offset = offset || 0;
+	label = label || '';
+	var d = new Date();
+	this.offset = (offset + d.getTimezoneOffset()) * 60 * 1000;
+
 	this.updateClock = function() {
 		var date = new Date();
+		var date = new Date(this.offset + date.getTime());
 		var clock = document.getElementById(id)
-		clock.innerHTML = this.formatDigits(date.getHours()) + ':' + this.formatDigits(date.getMinutes()) + ':' + this.formatDigits(date.getSeconds());
+		clock.innerHTML = this.formatDigits(date.getHours()) + ':' + this.formatDigits(date.getMinutes()) + ':' + this.formatDigits(date.getSeconds()) + ' ' + label;
 	};
+
 	this.formatDigits = function(val) {
 		if (val < 10) {
 			val =  '0' + val;
 		}
 		return val;
 	}
+
 	var that = this;
 	setInterval(function() {
 		that.updateClock();}, 1000);
