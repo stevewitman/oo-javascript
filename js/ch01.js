5b7da66e4dfec954acfca6b1c4195f4cf5ec0e80
Steve Witman <stevewitman@gmail.com>
2015-11-14 23:42:41 -0700

    add Date prototype

--- js/ch01.js
+++ js/ch01.js
@@ -4,35 +4,45 @@ function onReady() {
 	var clock = new Clock('clock3', 300, 'X');
 }
 
+Date.prototype.updateSeconds = function() {
+	this.setSeconds(this.getSeconds() + 1)
+}
+
+Date.prototype.autoClock = function(isAuto) {
+	clearInterval(this.clockInterval);
+	if(isAuto) {
+		var that = this;
+		this.clockInterval = setInterval(function(){that.updateSeconds()}, 1000);
+	}
+}
 
 function Clock(id, offset, label) {
 	offset = offset || 0;
 	label = label || '';
 	var d = new Date();
-	this.offset = (offset + d.getTimezoneOffset()) * 60 * 1000;
+	var offset = (offset + d.getTimezoneOffset()) * 60 * 1000;
+	this.d = new Date(offset + d.getTime());
+	this.d.autoClock(true);
 	this.id = id;
 	this.label = label;
 	var that = this;
 	setInterval(function() {
 		that.updateClock();}, 1000);
-		this.updateClock();
+	this.updateClock();
 }
 
-	Clock.prototype.updateClock = function() {
-		var date = new Date();
-		var date = new Date(this.offset + date.getTime());
-		var clock = document.getElementById(this.id)
-		clock.innerHTML = this.formatDigits(date.getHours()) + ':' + this.formatDigits(date.getMinutes()) + ':' + this.formatDigits(date.getSeconds()) + ' ' + this.label;
-	};
+Clock.prototype.updateClock = function() {
+	var date = this.d;
+	// date.updateSeconds();
+	var clock = document.getElementById(this.id)
+	clock.innerHTML = this.formatDigits(date.getHours()) + ':' + this.formatDigits(date.getMinutes()) + ':' + this.formatDigits(date.getSeconds()) + ' ' + this.label;
+};
 
-	Clock.prototype.formatDigits = function(val) {
-		if (val < 10) {
-			val =  '0' + val;
-		}
-		return val;
+Clock.prototype.formatDigits = function(val) {
+	if (val < 10) {
+		val =  '0' + val;
 	}
-
-
-
+	return val;
+}
 
 window.onload = onReady;
